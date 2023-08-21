641774f739a363953710671963ea545ca6cc9c56
Bryan Santos <49541500+bryandevelops@users.noreply.github.com>
2023-08-21 16:48:34 -0400

    Add onKeyDown event to Search Bar

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -12,7 +12,9 @@ export default function SearchBar ({ setMealData }) {
 
   return (
     <section className='search-bar-container'>
-      <input type='text' placeholder='Search for a meal...' ref={searchInput} />
+      <input type='text' placeholder='Search for a meal...' ref={searchInput} 
+        onKeyDown={(e) => { if (e.key === 'Enter') handleClick() }}
+      />
       <button onClick={handleClick}>Search</button>
     </section>
   );
