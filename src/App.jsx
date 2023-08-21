4c1ad76438a581498e1ab811d983b277ed832f06
Bryan Santos <49541500+bryandevelops@users.noreply.github.com>
2023-08-21 10:50:20 -0400

    Test an api function call

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
+import { useState, useEffect } from 'react';
+import { getRandomMeal } from './utils/api';
 import './App.css'
 
 export default function App() {
+  const [state, setState] = useState(null);
+
+  useEffect(() => {
+    getRandomMeal(setState)
+  }, [])
+
+  function loaded() {
+    return (
+      <>
+        <h1>{state.meals[0].strMeal}</h1>
+        <h2>{state.meals[0].strCategory}</h2>
+        <p>{state.meals[0].strInstructions}</p>
+        <img src={state.meals[0].strMealThumb}/>
+      </>
+    )
+  }
+
+  function loading() {
+    return <h1>Loading...</h1>
+  }
+
   return (
     <>
-      <h1>Loaded</h1>
+      {state ? loaded() : loading()}
     </>
   )
 }
