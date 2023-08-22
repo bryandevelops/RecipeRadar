c6fc90a50ed8793eeb88056999099cd7ae2c42ad
Bryan Santos <49541500+bryandevelops@users.noreply.github.com>
2023-08-22 17:31:07 -0400

    Create utility react component to automatically scroll the page to the top on route change

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import ScrollToTop from './utils/ScrollToTop.jsx';
 import App from './App.jsx';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
@@ -9,6 +10,7 @@ import './Main.css';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <App />
     </BrowserRouter>
   </StrictMode>
