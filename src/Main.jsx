354c2e73c7647effebeb635a512b73e7ee0ba20f
Bryan Santos <49541500+bryandevelops@users.noreply.github.com>
2023-08-21 20:49:19 -0400

    Import Material Design and Font Awesome CSS into Main

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import App from './App.jsx';
 import './Main.css';
 
