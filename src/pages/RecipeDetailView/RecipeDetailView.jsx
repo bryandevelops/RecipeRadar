28c5dbf7144dc17ae6f83d902f220cbba52b0ac2
Bryan Santos <49541500+bryandevelops@users.noreply.github.com>
2023-08-22 17:21:07 -0400

    Complete Recipe Detail View. Add preliminary styling

--- src/pages/RecipeDetailView/RecipeDetailView.jsx
+++ src/pages/RecipeDetailView/RecipeDetailView.jsx
@@ -3,11 +3,49 @@ import './RecipeDetailView.css';
 
 export default function RecipeDetailView (props) {
   const location = useLocation();
-  const { strMeal } = location.state;
-  console.log(location)
+  const { 
+    strMeal, strMealThumb, strCategory, 
+    strArea, strYoutube, strInstructions
+  } = location.state;
+
+  const ingredients = [];
+  let i = 1;
+  while (location.state[`strIngredient${i}`]) {
+    ingredients.push(
+      <li key={i}>{location.state[`strMeasure${i}`]} {location.state[`strIngredient${i}`]}</li>
+    )
+    i++;
+  }
+  
   return (
     <main className='recipe-detail-view'>
-      <h1>{strMeal}</h1>
+      <div className='top-half'>
+        <div className='meal-img-container'>
+          <img src={strMealThumb} alt={strMeal} width={400}/>
+        </div>
+        <div className='meal-info-container'>
+          <h1>{strMeal}</h1>
+          <h2>{strCategory}</h2>
+          <h3>{strArea}</h3>
+          <ul>
+            {ingredients}
+          </ul>
+        </div>
+      </div>
+
+      <div>
+        <h1>Video</h1>
+        <iframe 
+          src={`https://www.youtube.com/embed/${strYoutube.split("=")[1]}`}
+          width="420" height="315"
+          title={strMeal}>
+        </iframe>
+      </div>
+      
+      <div>
+        <h1>Instructions</h1>
+        <p className='instructions'>{strInstructions}</p>
+      </div>
     </main>
   );
 }
