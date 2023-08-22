c0c68a2cdcfb613a178025055b6bb0d2d0cba0cb
Bryan Santos <49541500+bryandevelops@users.noreply.github.com>
2023-08-21 20:49:51 -0400

    Change import order

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import App from './App.jsx';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import App from './App.jsx';
 import './Main.css';
 
 createRoot(document.getElementById('root')).render(
