2c97c6e2bb8714ce19c384dd4d7d9b4e4b8a9af5
Bryan Santos <49541500+bryandevelops@users.noreply.github.com>
2023-08-23 17:44:31 -0400

    Add country codes object to api file

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,3 +1,34 @@
+export const countryCodes = {
+  American: 'us',
+  British: 'gb',
+  Canadian: 'ca',
+  Chinese: 'cn',
+  Croatian: 'hr',
+  Dutch: 'nl',
+  Egyptian: 'eg',
+  Filipino: 'ph',
+  French: 'fr',
+  Greek: 'gr',
+  Indian: 'in',
+  Irish: 'ie',
+  Italian: 'it',
+  Jamaican: 'jm',
+  Japanese: 'jp',
+  Kenyan: 'kn',
+  Malaysian: 'my',
+  Mexican: 'mx',
+  Moroccan: 'ma',
+  Polish: 'pl',
+  Portuguese: 'pt',
+  Russian: 'ru',
+  Spanish: 'es',
+  Thai: 'th',
+  Tunisian: 'tn',
+  Turkish: 'tr',
+  Unknown: '',
+  Vietnamese: 'vn'
+}
+
 export async function getMealsBySearch(setState, value) {
   try {
     const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${value}`;
